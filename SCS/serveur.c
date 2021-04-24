cca1ba061f76087a7209fccc9fd56acc4ef25c6d
KaramHnni <karam.tenes@gmail.com>
2021-04-24 15:22:03 +0200

    work on server

--- SCS/serveur.c
+++ SCS/serveur.c
@@ -1,8 +1,9 @@
 #include "fonctionsTCP.h"
 #include "protocolNewton.h"
 #include "validation.h"
+#include "time.h"
 
-
+#define TIME_MAX 6;
 
 int main(int argc, char** argv) {
   int  sockConx,        /* descripteur socket connexion */
@@ -12,17 +13,19 @@ int main(int argc, char** argv) {
        err;
 
   TCodeReq codeReq;
+  
+  TCoupReq reqCoup;
+  TCoupRep repCoup;
   TPartieReq reqPartie;
   TPartieRep repPartieJ1, repPartieJ2;
-  int joueur1 = 0;
-  int joueur2 = 0;
+  TPropCoup *propCoup;
+
+  int numPartie = 1;
   char nomJoueur1[T_NOM];
   char nomJoueur1[T_NOM];
 
   struct sockaddr_in addClient;	/* adresse de la socket client connectee */
-  
 
-  
   /*
    * verification des arguments
    */
@@ -37,30 +40,23 @@ int main(int argc, char** argv) {
   
   sizeAddr = sizeof(struct sockaddr_in);
 
-  fd_set  readSet;
 
 
   sockTrans1 = accept(sockConx, NULL, NULL);
-  if (sockTrans1 < 0) { /* erreur … */ }
   sockTrans2 = accept(sockConx, NULL, NULL);
-  if (sockTrans2 < 0) { /* erreur … */ }
-  
 
-  FD_ZERO(&readSet);
-  FD_SET(sockTrans1, &readSet);
-  FD_SET(sockTrans2, &readSet);
-  nfsd = 6;
-  err = select(nfsd, &readSet, NULL, NULL, NULL);
+  if (sockTrans1 < 0) { /* erreur … */ }
+  if (sockTrans2 < 0) { /* erreur … */ }
+
+
   if (err < 0) {
       perror("(serveurSelect) erreur dans select");
       /* shutdown + close */
   }
 
-  while(1){
+  
 
-  if (FD_ISSET(sockTrans1, &readSet) != 0) { 
-     
-    err = recv(sockTrans1, &codeReq, sizeof(TCodeReq), 0);
+    err = recv(sockTrans1, &codeReq, sizeof(TCodeReq), MSG_PEEK);
     if (err < 0 ) 
     perror("(serveurSelect) erreur dans le recv 1");
 
@@ -69,21 +65,18 @@ int main(int argc, char** argv) {
         case PARTIE : 
         
         err = recv(sockTrans1, &reqPartie,sizeof(TPartieReq), 0);
-
-        if(joueur1 == 0){
-            joueur1 = 1;
             repPartieJ1.err = ERR_OK;
             repPartieJ1.coulPion = BLEU;
             nomJoueur1 = reqPartie.nomJoueur;
+            break;
+
         }
+        case default : 
+        repPartieJ1.err = ERR_TYP;
         break;
-    }
-  }
 
-
-   if (FD_ISSET(sockTrans2, &readSet) != 0) { 
-     
-    err = recv(sockTrans2, &codeReq, sizeof(TCodeReq), 0);
+    
+    err = recv(sockTrans2, &codeReq, sizeof(TCodeReq), MSG_PEEK);
     if (err < 0 ) 
     perror("(serveurSelect) erreur dans le recv 1");
 
@@ -91,43 +84,459 @@ int main(int argc, char** argv) {
 
         case PARTIE : 
         
-        err = recv(sockTrans1, &reqPartie,sizeof(TPartieReq), 0);
-
-        if(joueur1 == 1){
-            joueur2 = 1;
+        err = recv(sockTrans2,&reqPartie,sizeof(TPartieReq), 0);
             repPartieJ2.err = ERR_OK;
             repPartieJ2.coulPion = ROUGE;
             nomJoueur2 = reqPartie.nomJoueur;
-        }
+            break;
+
+
+      case default : 
+
+        repPartieJ2.err = ERR_TYP;
         break;
     }
-  }
-
-
-  if(joueur1 == 1 && joueur2 == 1){
+  
       initialiserPartie();
       repPartieJ1.nomAdvers = nomJoueur2;
       repPartieJ2.nomAdvers = nomJoueur1;
-     err = send(sockTrans1, repPartieJ1, sizeof(TPartieRep) , 0);
+      err = send(sockTrans1, repPartieJ1, sizeof(TPartieRep) , 0);
       if (err <= 0) { 
       perror("(serveurR) erreur sur le send");
-      shutdown(sockTrans, SHUT_RDWR); close(sockTrans);
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
 
       err = send(sockTrans2, repPartieJ2, sizeof(TPartieRep) , 0);
       if (err <= 0) { 
       perror("(serveurR) erreur sur le send");
-      shutdown(sockTrans, SHUT_RDWR); close(sockTrans);
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
 
-  }
-}
+
+    if(numPartie == 1){
+
+     while(1){
+
+          
+          struct timeval tv;
+          tv.tv_sec = timeout;
+          tv.tv_usec = 0;
+          setsockopt(sockTrans1, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+          err = recv(sockTrans1, &reqCoup,sizeof(codeReq), MSG_PEEK);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+            return -5;
+
+          }
+
+          if(reqCoup == COUP){
+
+          err = recv(sockTrans1, &reqCoup,sizeof(TCoupReq), 0);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+            return -5;
+
+          }
+
+      bool coupBool = validationCoup(1,reqCoup, &propCoup)
+      if(coupBool == false && reqCoup.propCoup == CONT){
+            
+          repCoup.err = ERR_COUP;
+          repCoup.validCoup = TRICHE;
+          repCoup.propCoup = PERDU;
+
+      err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+          
+            printf("Coup Non valide");
+
+            break;
+            /*
+              Do something about the socket
+            */
+          }else{
+
+          repCoup.err = ERR_OK;
+          repCoup.validCoup = VALID;
+          repCoup.propCoup = propCoup;
+
+      err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+    }
+
+      // envoie de la validité du coup au joueur 2 
+
+      err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      if(coupBool == true && propCoup != CONT){
+
+        printf("La partie est en etat %c",propCoup);
+        break;
+      }
+      if(coupBool == true && reqCoup.propCoup == CONT){
+
+      err = send(sockTrans2, reqCoup, sizeof(TCoupReq) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      }
+          }
+          else{
+
+              repCoup.err = ERR_TYP;
+              err = send(sockTrans1, repCoup, sizeof(TCodeRep) , 0);
+              if (err <= 0) { 
+              perror("(serveurR) erreur sur le send");
+              shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+              return -5;
+              }
+              break;
+
+          }
+      /*-------------- Recevoir le coup du joueur 2 ---------- */
+    
+
+          setsockopt(sockTrans2, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+          err = recv(sockTrans2, &reqCoup,sizeof(codeReq), MSG_PEEK);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+            return -5;
+
+          }
+
+          if(reqCoup == COUP){
+
+          err = recv(sockTrans2, &reqCoup,sizeof(TCoupReq), 0);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+            return -5;
+
+          }
+
+      bool coupBool = validationCoup(2,reqCoup, &propCoup)
+      if(coupBool == false && reqCoup.propCoup == CONT){
+            
+          repCoup.err = ERR_COUP;
+          repCoup.validCoup = TRICHE;
+          repCoup.propCoup = PERDU;
+
+          err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+        printf("Coup Non valide, Joueur perdant");
+
+      break;
+        
+      }else{
+       repCoup.err = ERR_OK;
+       repCoup.validCoup = VALID;
+       repCoup.propCoup = propCoup;
+
+     err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+      }
+      
+
+      // envoie de la validité du coup au joueur 1 
+
+      err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      if(coupBool == true && reqCoup.propCoup == CONT){
+
+      err = send(sockTrans1, reqCoup, sizeof(TCoupReq) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+
+      }
+        
+
+
+
+      
+          }
+          else{
+
+              repCoup.err = ERR_TYP;
+              err = send(sockTrans2, repCoup, sizeof(TCodeRep) , 0);
+              if (err <= 0) { 
+              perror("(serveurR) erreur sur le send");
+              shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+              return -5;
+              }
+              break;
+
+          }
+      
+     }
+
+      numPartie++;
+  
+    }
+  
+    if(numPartie == 2){
+
+      while(1){
+
+         struct timeval tv;
+          tv.tv_sec = timeout;
+          tv.tv_usec = 0;
+          setsockopt(sockTrans2, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+          err = recv(sockTrans2, &reqCoup,sizeof(codeReq), MSG_PEEK);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+            return -5;
+
+          }
+
+          if(reqCoup == COUP){
+
+          err = recv(sockTrans2, &reqCoup,sizeof(TCoupReq), 0);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+            return -5;
+
+          }
+
+      bool coupBool = validationCoup(2,reqCoup, &propCoup)
+      if(coupBool == false && reqCoup.propCoup == CONT){
+            
+          repCoup.err = ERR_COUP;
+          repCoup.validCoup = TRICHE;
+          repCoup.propCoup = PERDU;
+
+      err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+          
+            printf("Coup Non valide");
+
+            break;
+            /*
+              Do something about the socket
+            */
+          }else{
+
+          repCoup.err = ERR_OK;
+          repCoup.validCoup = VALID;
+          repCoup.propCoup = propCoup;
+
+      err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+    }
+
+      // envoie de la validité du coup au joueur 2 
+
+      err = send(sockTrans2, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      if(coupBool == true && propCoup != CONT){
+
+        printf("La partie est en etat %c",propCoup);
+        break;
+      }
+      if(coupBool == true && reqCoup.propCoup == CONT){
+
+      err = send(sockTrans2, reqCoup, sizeof(TCoupReq) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      }
+          }
+          else{
+
+              repCoup.err = ERR_TYP;
+              err = send(sockTrans2, repCoup, sizeof(TCodeRep) , 0);
+              if (err <= 0) { 
+              perror("(serveurR) erreur sur le send");
+              shutdown(sockTrans2, SHUT_RDWR); close(sockTrans);
+              return -5;
+              }
+              break;
+
+          }
+      /*-------------- Recevoir le coup du joueur 1 ---------- */
+    
+
+          setsockopt(sockTrans1, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+          err = recv(sockTrans1, &reqCoup,sizeof(codeReq), MSG_PEEK);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+            return -5;
+
+          }
+
+          if(reqCoup == COUP){
+
+          err = recv(sockTrans1, &reqCoup,sizeof(TCoupReq), 0);
+          if(err <= 0){
+
+            perror("(serveurR) erreur sur le send");
+            shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+            return -5;
+
+          }
+
+      bool coupBool = validationCoup(1,reqCoup, &propCoup)
+      if(coupBool == false && reqCoup.propCoup == CONT){
+            
+          repCoup.err = ERR_COUP;
+          repCoup.validCoup = TRICHE;
+          repCoup.propCoup = PERDU;
+
+          err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+        printf("Coup Non valide, Joueur perdant");
+
+      break;
+        
+      }else{
+       repCoup.err = ERR_OK;
+       repCoup.validCoup = VALID;
+       repCoup.propCoup = propCoup;
+
+     err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+      }
+      
+
+      // envoie de la validité du coup au joueur 1 
+
+      err = send(sockTrans1, repCoup, sizeof(TCoupRep) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+      return -5;
+      }
+
+      if(coupBool == true && reqCoup.propCoup == CONT){
+
+      err = send(sockTrans2, reqCoup, sizeof(TCoupReq) , 0);
+      if (err <= 0) { 
+      perror("(serveurR) erreur sur le send");
+      shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+      return -5;
+      }
+
+      }
+        
+
+
+
+      
+          }
+          else{
+
+              repCoup.err = ERR_TYP;
+              err = send(sockTrans1, repCoup, sizeof(TCodeRep) , 0);
+              if (err <= 0) { 
+              perror("(serveurR) erreur sur le send");
+              shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+              return -5;
+              }
+              break;
+
+          }
+      
+     }
+
+      }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /* 
-   * arret de la connexion et fermeture
+   * arret de la connexion et fermeture -- Tout a la fin du programme ! 
    */
-  shutdown(sockTrans, SHUT_RDWR); close(sockTrans);
+
+
+  
+  shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
+  shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
+
   close(sockConx);
   
   
