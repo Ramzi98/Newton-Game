aa5f362026ce5af7a7972b2e95404099f941cf24
karamHnni <karam.tenes@gmail.com>
2021-05-01 15:08:53 +0200

    work on server

--- SCS/serveur.c
+++ SCS/serveur.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
        port,            /* numero de port */
        sizeAddr,        /* taille de l'adresse d'une socket */
        err,
-       nfsd;
+       nfsd =6;
        fd_set readSet;
 
  struct timeval tv;
@@ -30,46 +30,88 @@ tv.tv_usec = 0;
   int numPartie = 1;
   char nomJoueur1[T_NOM];
   char nomJoueur2[T_NOM];
+  bool timeout = true; //Avec timeout
+  bool validation = true; //avec timeout
+  char* time = "--noTimeout";
+  char* valid = "--noValid";
+  bool waitPartie1 = true;
+  bool waitPartie2 = false;
+  bool selectionPartie = false;
+  int retval;
+  
+  if (argc >= 2 || argc <= 4) {
+    	if(argc == 2) port  = atoi(argv[1]);
+  		
+  		else if(argc == 3 && ( !(strcmp(argv[1],time)) || !(strcmp(argv[1],valid)) )) {
+  			port  = atoi(argv[2]);
+
+  			if (!(strcmp(argv[1],time))) timeout = false;  				
+  			if (!(strcmp(argv[1],valid))) validation = false;
+  	
+  		}
+  		else if(argc == 4 && (!(strcmp(argv[1],time)) || !(strcmp(argv[1],valid))) &&
+  		 (!(strcmp(argv[2],time)) || !(strcmp(argv[2],valid))) && (strcmp(argv[1],argv[2]))){
+  			port  = atoi(argv[3]);
+  			if (!(strcmp(argv[1],time)) || !(strcmp(argv[2],time))) timeout = false;
+  			if (!(strcmp(argv[1],valid)) || !(strcmp(argv[2],valid))) validation = false;
+  				
+  		}
+  		else{
+  			printf("usage : ./serveur [--noValid|--noTimeout] no_port\n");
+  			return -1;
+  		}
+    
+  	}
+  	else{
+  		printf("usage : ./serveur [--noValid|--noTimeout] no_port\n");
+  		return -2;
+  	}
+
+  	if (timeout && validation ) printf("Starting ./serveur on port %d with validation and timeout \n",port);
+  	else if (!timeout && validation) printf("Starting ./serveur on port %d with validation and  no timeout \n",port);
+  	else if (timeout && !validation) printf("Starting ./serveur on port %d with no validation and timeout \n",port);
+  	else if (!timeout && !validation) printf("Starting ./serveur on port %d with no validation and  no timeout \n",port);
+  
+
 
   struct sockaddr_in addClient;	/* adresse de la socket client connectee */
 
-  /*
-   * verification des arguments
-   */
-  if (argc != 2) {
-    printf ("usage : %s port\n", argv[0]);
-    return -1;
-  }
-  
-  port  = atoi(argv[1]);
-  
+
   sockConx = socketServeur(port);
   
   sizeAddr = sizeof(struct sockaddr_in);
 
-
-
   sockTrans1 = accept(sockConx, NULL, NULL);
+
+    if (sockTrans1 < 0) { printf("error sur accept trans1\n"); return 2; }
   sockTrans2 = accept(sockConx, NULL, NULL);
+  if (sockTrans2 < 0) { printf("error sur accept trans2\n"); return 2;}
 
-  if (sockTrans1 < 0) { /* erreur … */ }
-  if (sockTrans2 < 0) { /* erreur … */ }
-
-
-  if (err < 0) {
-      perror("(serveurSelect) erreur dans select");
-      /* shutdown + close */
-  }
 
   FD_ZERO(&readSet) ;  // initialisation à zéro
   FD_SET(sockTrans1, &readSet);
   FD_SET(sockTrans2, &readSet);
 
+
+  err = select(nfsd,&readSet,NULL,NULL,NULL);
+
+  if(err<0){
+  	printf("error in select\n");
+  	return 3;
+  	/*CodeErreur*/
+  }
   
 
+  while(!selectionPartie){
+  
+  FD_ZERO(&readSet) ;  // initialisation à zéro
+  FD_SET(sockTrans1, &readSet);
+  FD_SET(sockTrans2, &readSet);
+	
+ if(FD_ISSET(sockTrans1,&readSet)!=0 && waitPartie1){
     err = recv(sockTrans1, &codeReq, sizeof(TIdReq), MSG_PEEK);
     if (err < 0 ) 
-    perror("(serveurSelect) erreur dans le recv 1");
+    perror("(serveurSelect) erreur dans le recv 1\n");
 
     switch(codeReq){
 
@@ -86,11 +128,13 @@ tv.tv_usec = 0;
         repPartieJ1.err = ERR_TYP;
         break;
     }
-
-    
+    waitPartie1= false;
+    waitPartie2 = true;
+   }
+  if(FD_ISSET(sockTrans2,&readSet)!=0 && waitPartie2){
     err = recv(sockTrans2, &codeReq, sizeof(TIdReq), MSG_PEEK);
     if (err < 0 ) 
-    perror("(serveurSelect) erreur dans le recv 1");
+    perror("(serveurSelect) erreur dans le recv 1\n");
 
     switch(codeReq){
 
@@ -108,35 +152,46 @@ tv.tv_usec = 0;
         repPartieJ2.err = ERR_TYP;
         break;
     }
-  
-      initialiserPartie();
+    
+    waitPartie2 = false;
+    selectionPartie = true;
+ 
+ }
+ 
+ }
+ 
+
+ if(!waitPartie1 && !waitPartie2){
+
       memcpy(repPartieJ1.nomAdvers, nomJoueur2, T_NOM );  
-      memcpy(repPartieJ1.nomAdvers, nomJoueur2, T_NOM );  
+      memcpy(repPartieJ2.nomAdvers, nomJoueur1, T_NOM );  
       err = send(sockTrans1, &repPartieJ1, sizeof(TPartieRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
 
       err = send(sockTrans2, &repPartieJ2, sizeof(TPartieRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
-      }
+      } 
+   }
 
 
     if(numPartie == 1){
-
+	
+	initialiserPartie();
+        printf("On commence la partie avec %s en bleu\n",nomJoueur1);
      while(1){
 
-          int retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
-          if(retval <= 0 ){
+         // int retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
+          /*if(retval <= 0 ){
 
             printf("error");
-            /*send response timeout*/
-          }
+            /*send response timeout }*/
           err = recv(sockTrans1, &codeReq,sizeof( TIdReq), MSG_PEEK);
           if(err <= 0){
 
@@ -148,12 +203,13 @@ tv.tv_usec = 0;
           switch(codeReq){
           case COUP: 
 
-         retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
+         /*retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
-            /*send response timeout*/
+            printf("error\n");
+            send response timeout
           }
+          */
           err = recv(sockTrans1, &reqCoup,sizeof(TCoupReq), 0);
           if(err <= 0){
 
@@ -164,44 +220,30 @@ tv.tv_usec = 0;
           }
 
       bool coupBool = validationCoup(1,reqCoup, &propCoup);
+
       if(coupBool == false && reqCoup.propCoup == CONT){
             
           repCoup.err = ERR_COUP;
           repCoup.validCoup = TRICHE;
           repCoup.propCoup = PERDU;
-
-      err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
-      if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
-      shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
-      return -5;
-      }
-          
-            printf("Coup Non valide");
-
-            break;
-            /*
-              Do something about the socket
-            */
-          }else{
-
+	}else{
+	printf("on est la 1\n");
           repCoup.err = ERR_OK;
           repCoup.validCoup = VALID;
           repCoup.propCoup = *propCoup;
-
+          printf("Coup Non valide\n");
+        }
       err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(CLient 1 erreur sur le send");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
-    }
-
+      printf("on est la 1\n");
       // envoie de la validité du coup au joueur 2 
-
       err = send(sockTrans2, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(CLient 2) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
@@ -211,17 +253,18 @@ tv.tv_usec = 0;
       //  printf("La partie est en etat %s",propCoup);
         break;
       }
+      printf("on est la 2\n");
       if(coupBool == true && reqCoup.propCoup == CONT){
 
       err = send(sockTrans2, &reqCoup, sizeof(TCoupReq) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
-
+	printf("on est la \n");
       }
-          break;
+     break;
 
           default :
           
@@ -229,7 +272,7 @@ tv.tv_usec = 0;
               repCoup.err = ERR_TYP;
               err = send(sockTrans1, &repCoup, sizeof(TCodeRep) , 0);
               if (err <= 0) { 
-              perror("(serveurR) erreur sur le send");
+              perror("(serveurR) erreur sur le send\n");
               shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
               return -5;
               }
@@ -238,17 +281,17 @@ tv.tv_usec = 0;
           }
       /*-------------- Recevoir le coup du joueur 2 ---------- */
     
-
+	printf("on est la \n");
          retval = select(nfsd, &sockTrans2, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
+            printf("error\n");
             /*send response timeout*/
           }         
          err = recv(sockTrans2, &codeReq,sizeof(TIdReq), MSG_PEEK);
           if(err <= 0){
 
-            perror("(serveurR) erreur sur le send");
+            perror("(serveurR) erreur sur le send\n");
             shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
             return -5;
 
@@ -260,7 +303,7 @@ tv.tv_usec = 0;
            retval = select(nfsd, &sockTrans2, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
+            printf("error\n");
             /*send response timeout*/
           }
           err = recv(sockTrans2, &reqCoup,sizeof(TCoupReq), 0);
@@ -285,7 +328,7 @@ tv.tv_usec = 0;
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
-        printf("Coup Non valide, Joueur perdant");
+        printf("Coup Non valide, Joueur perdant\n");
 
       break;
         
@@ -296,7 +339,7 @@ tv.tv_usec = 0;
 
      err = send(sockTrans2, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
@@ -358,14 +401,14 @@ tv.tv_usec = 0;
         int retval = select(nfsd, &sockTrans2, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
+            printf("error\n");
             /*send response timeout*/
           }
 
           err = recv(sockTrans2, &codeReq,sizeof(TIdReq), MSG_PEEK);
           if(err <= 0){
 
-            perror("(serveurR) erreur sur le send");
+            perror("(serveurR) erreur sur le send\n");
             shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
             return -5;
 
@@ -377,13 +420,13 @@ tv.tv_usec = 0;
          retval = select(nfsd, &sockTrans2, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
+            printf("error\n");
             /*send response timeout*/
           }
           err = recv(sockTrans2, &reqCoup,sizeof(TCoupReq), 0);
           if(err <= 0){
 
-            perror("(serveurR) erreur sur le send");
+            perror("(serveurR) erreur sur le send\n");
             shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
             return -5;
 
@@ -398,12 +441,12 @@ tv.tv_usec = 0;
 
       err = send(sockTrans2, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
           
-            printf("Coup Non valide");
+            printf("Coup Non valide\n");
 
             break;
             /*
@@ -417,7 +460,7 @@ tv.tv_usec = 0;
 
       err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
@@ -427,7 +470,7 @@ tv.tv_usec = 0;
 
       err = send(sockTrans2, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
@@ -441,7 +484,7 @@ tv.tv_usec = 0;
 
       err = send(sockTrans2, &reqCoup, sizeof(TCoupReq) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
@@ -453,7 +496,7 @@ tv.tv_usec = 0;
               repCoup.err = ERR_TYP;
               err = send(sockTrans2, &repCoup, sizeof(TCodeRep) , 0);
               if (err <= 0) { 
-              perror("(serveurR) erreur sur le send");
+              perror("(serveurR) erreur sur le send\n");
               shutdown(sockTrans2, SHUT_RDWR); close(&sockTrans2);
               return -5;
               }
@@ -467,13 +510,13 @@ tv.tv_usec = 0;
          retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
                   if(retval <= 0 ){
 
-                    printf("error");
+                    printf("error\n");
                     /*send response timeout*/
                   }
           err = recv(sockTrans1, &codeReq,sizeof(TIdReq), MSG_PEEK);
           if(err <= 0){
 
-            perror("(serveurR) erreur sur le send");
+            perror("(serveurR) erreur sur le send\n");
             shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
             return -5;
 
@@ -485,13 +528,13 @@ tv.tv_usec = 0;
            retval = select(nfsd, &sockTrans1, NULL, NULL, &tv); 
           if(retval <= 0 ){
 
-            printf("error");
+            printf("error\n");
             /*send response timeout*/
           }
           err = recv(sockTrans1, &reqCoup,sizeof(TCoupReq), 0);
           if(err <= 0){
 
-            perror("(serveurR) erreur sur le send");
+            perror("(serveurR) erreur sur le send\n");
             shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
             return -5;
 
@@ -506,11 +549,11 @@ tv.tv_usec = 0;
 
           err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
-        printf("Coup Non valide, Joueur perdant");
+        printf("Coup Non valide, Joueur perdant\n");
 
       break;
         
@@ -521,7 +564,7 @@ tv.tv_usec = 0;
 
      err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
@@ -532,7 +575,7 @@ tv.tv_usec = 0;
 
       err = send(sockTrans1, &repCoup, sizeof(TCoupRep) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
       return -5;
       }
@@ -541,7 +584,7 @@ tv.tv_usec = 0;
 
       err = send(sockTrans2, &reqCoup, sizeof(TCoupReq) , 0);
       if (err <= 0) { 
-      perror("(serveurR) erreur sur le send");
+      perror("(serveurR) erreur sur le send\n");
       shutdown(sockTrans2, SHUT_RDWR); close(sockTrans2);
       return -5;
       }
@@ -554,7 +597,7 @@ tv.tv_usec = 0;
               repCoup.err = ERR_TYP;
               err = send(sockTrans1, &repCoup, sizeof(TCodeRep) , 0);
               if (err <= 0) { 
-              perror("(serveurR) erreur sur le send");
+              perror("(serveurR) erreur sur le send\n");
               shutdown(sockTrans1, SHUT_RDWR); close(sockTrans1);
               return -5;
               }
